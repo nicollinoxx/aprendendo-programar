f3436487f003a5226ac7e3f109ec8ae31b297764
Nicolas Almeida <nicolasalmeida321@gmail.com>
2020-05-06 12:09:54 -0300

    fdsa

--- calc.rb
+++ calc.rb
@@ -1,16 +1,2 @@
-puts 1 > 2
-puts 1 < 2
-
-puts 6 <= 5
-puts 6 <= 6
-
-puts 7 == 7
-puts 7 != 9
-
-puts 'Óla meu amigo eu sou um vindente acredita?, por favor me diga seu nome'
-nome = gets.chomp
-if nome == 'Jacare'
-puts 'Nossa seu futuro sera uma beleza meu amigo haha'
 else
-puts 'Nossa eu lamento dizer mais seu fututo sera uma bosta'
 end
