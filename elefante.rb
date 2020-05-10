9063bc0b881b1e1d87a041f96e342ccb6b3f2c88
Nicolas Almeida <nicolasalmeida321@gmail.com>
2020-05-10 14:48:56 -0300

    elefante segunda versao

--- elefante.rb
+++ elefante.rb
@@ -1,17 +1,5 @@
-comando = ''
-puts ''
-while comando != 'tchau'
-  comando = gets.chomp
-  if comando == '10 elefantes incomoda muita gente'
-    puts '20 elefantes incomoda muita gente'
-    puts '30 elefantes incomoda muita gente'
-    puts '40 elefantes incomoda muita gente'
-    puts '50 elefantes incomoda muita gente'
-  end
-  if comando == '60 elefantes incomoda muita gente'
-    puts '70 elefantes incomoda muita gente'
-    puts '80 elefantes incomoda muita gente'
-    puts '90 elefantes incomoda muita gente'
-    puts '100 elefantes incomoda muita gente'
-  end
+quantidade = 0
+while quantidade != 100
+  quantidade = quantidade + 1
+  puts quantidade.to_s + ' elefantes incomoda muita gente'
 end
