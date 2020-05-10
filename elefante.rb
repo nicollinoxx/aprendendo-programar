bc78ab194eb79dce495c0e192b2d97fc5e908085
Nicolas Almeida <nicolasalmeida321@gmail.com>
2020-05-10 16:30:09 -0300

    elefeante quarta versao

--- elefante.rb
+++ elefante.rb
@@ -1,12 +1,12 @@
 quantidade = 0
-while quantidade != 100
-  quantidade = quantidade + 1
-  resto = quantidade % 2
 
-  if resto != 0
+while quantidade < 100
+  quantidade = quantidade + 1
+  resto_da_divisao = quantidade % 2
+
+  if resto_da_divisao != 0
     puts quantidade.to_s + ' elefantes incomoda muita gente'
   else
-    incomodam = ' incomoda' * quantidade
-    puts quantidade.to_s + ' elefantes' + incomodam  + ' muito mais'
+    puts quantidade.to_s + ' elefantes' + (' incomoda' * quantidade)  + ' muito mais'
   end
 end
