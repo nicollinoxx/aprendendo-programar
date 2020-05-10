67548727c10698d6928dde28f280cb71221a8d43
Nicolas Almeida <nicolasalmeida321@gmail.com>
2020-05-10 14:19:35 -0300

    velha identado

--- calc.rb
+++ calc.rb
@@ -1,23 +1,23 @@
 comando = ''
 while comando != 'TCHAU'
-puts ''
+  puts ''
   puts 'A velha surda'
   puts 'O que voce que meu filho?'
-puts ''
-comando = gets.chomp
+  puts ''
+  comando = gets.chomp
   if comando == comando.upcase
-  puts 'NAO DESDE 1938'
-else
-  if comando == comando.downcase
-  puts 'NAO DESDE 1950'
-else
-  puts 'nao estou ouvindo'
-  puts 'DIGA MAIS ALTO MEU FILHO'
-  if comando != 'Bom dia senhora'
-  puts 'AINDA NAO ESTOU OUVINDO'
-else
-  puts 'FALE MAIS ALTO'
-end
-end
-end
+    puts 'NAO DESDE 1938'
+  else
+    if comando == comando.downcase
+      puts 'NAO DESDE 1950'
+    else
+      puts 'nao estou ouvindo'
+      puts 'DIGA MAIS ALTO MEU FILHO'
+      if comando != 'Bom dia senhora'
+        puts 'AINDA NAO ESTOU OUVINDO'
+      else
+        puts 'FALE MAIS ALTO'
+      end
+    end
+  end
 end
