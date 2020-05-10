2e65f9feacc59a32817ae89a7e372539ee23fa75
Nicolas Almeida <nicolasalmeida321@gmail.com>
2020-05-10 14:26:57 -0300

    elefante primera versao

---
+++ elefante.rb
@@ -0,0 +1,17 @@
+comando = ''
+puts ''
+while comando != 'tchau'
+  comando = gets.chomp
+  if comando == '10 elefantes incomoda muita gente'
+    puts '20 elefantes incomoda muita gente'
+    puts '30 elefantes incomoda muita gente'
+    puts '40 elefantes incomoda muita gente'
+    puts '50 elefantes incomoda muita gente'
+  end
+  if comando == '60 elefantes incomoda muita gente'
+    puts '70 elefantes incomoda muita gente'
+    puts '80 elefantes incomoda muita gente'
+    puts '90 elefantes incomoda muita gente'
+    puts '100 elefantes incomoda muita gente'
+  end
+end
